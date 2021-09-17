65fe7ccda375b6e5441da6377e6520ea584f29d3
AviDoesDev <81582444+AviDoesDev@users.noreply.github.com>
2021-09-17 15:54:22 +0530

    Added express webserver

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const { exec } = require('child_process');
-const fs = require('fs').promises;
-module.exports = async (string) => {
-    const process = exec(
-        "python " + __dirname + "\\tts.py",
-        {
-            stdio: ['pipe', 'pipe', 'pipe']
-        }
-    );
-    const file = `${Math.floor(Math.random() * 1000)}.wav`;
+require('dotenv').config();
 
-    await new Promise((res) => {
-        process.stdout.on('data', () => res());
-    });
+const express = require('express');
+const rateLimit = require("express-rate-limit");
+const tts = require('./tts');
 
-    process.stdin.write(string + "\n");
-    process.stdin.write(file + "\n");
+const app = express();
+app.set('trust proxy', process.env.PROXY || 0);
 
-    await new Promise((res) => {
-        process.stdout.on('data', () => res());
-    });
+app.use(rateLimit({
+    windowMs: 60 * 1000,
+    max: 25
+}));
 
-    const result = await fs.readFile(file);
-    fs.unlink(file);
-    return result;
-}
+app.get('/', async (req, res) => {
+    if (!req.query.text) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify({
+            'error': 'Missing text parameter'
+        }));
+    }
+
+    const text = req.query.text;
+    const voice = req.query.female == 'true';
+    const response = await tts(text, voice);
+    res.writeHead(200, { 'Content-Type': 'audio/mpeg' });
+    return res.end(response);
+});
+
+app.listen(process.env.PORT || 8080, () => console.log(`Server started on port: ${process.env.PORT || 8080}`))
---
+++ tts.js
@@ -0,0 +1,27 @@
+const { exec } = require('child_process');
+const fs = require('fs').promises;
+module.exports = async (string, voice = true) => {
+    const process = exec(
+        "python " + __dirname + "\\tts.py",
+        {
+            stdio: ['pipe', 'pipe', 'pipe']
+        }
+    );
+    const file = `${Math.floor(Math.random() * 1000)}.wav`;
+
+    await new Promise((res) => {
+        process.stdout.on('data', () => res());
+    });
+
+    process.stdin.write(string + "\n");
+    process.stdin.write(file + "\n");
+    process.stdin.write((voice == true ? 2 : 0) + "\n");
+
+    await new Promise((res) => {
+        process.stdout.on('data', () => res());
+    });
+
+    const result = await fs.readFile(file);
+    fs.unlink(file);
+    return result;
+}
